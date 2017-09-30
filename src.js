66b81c07dfb172c9499a0e883804085457fa3f74
An Cao <kcj.pop.com@gmail.com>
2017-10-01 00:00:17 +0300

    Show word count and estimated reading time
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src.js
+++ src.js
@@ -6,35 +6,45 @@ const layouts = require('metalsmith-layouts')
 const markdown = require('metalsmith-markdown')
 const permalinks = require('metalsmith-permalinks')
 const collections = require('metalsmith-collections')
+const wordcount = require('metalsmith-word-count')
 
 const OUTPUT_PATH = path.resolve(__dirname, 'dist')
 
 module.exports = Metalsmith(__dirname)
   .source(path.resolve(__dirname, 'src/content'))
   .destination(OUTPUT_PATH)
-  .use(asset({
-    src: './src/assets',
-    dest: '.'
-  }))
-  .use(collections({
-    lastArticles: {
-      pattern: 'posts/*.md',
-      sortBy: 'date',
-      refer: false,
-      reverse: true
-    }
-  }))
+  .use(
+    asset({
+      src: './src/assets',
+      dest: '.'
+    })
+  )
+  .use(
+    collections({
+      lastArticles: {
+        pattern: 'posts/*.md',
+        sortBy: 'date',
+        refer: false,
+        reverse: true
+      }
+    })
+  )
   .use(markdown({ langPrefix: 'language-' }))
-  .use(permalinks({
-    pattern: ':language/:slug'
-  }))
-  .use(dates({
-    dates: [
-      { key: 'date', format: 'DD/MM/YYYY' }
-    ]
-  }))
-  .use(layouts({
-    engine: 'handlebars',
-    directory: path.resolve(__dirname, 'src/layouts'),
-    partials: path.resolve(__dirname, 'src/layouts/el')
-  }))
+  .use(
+    permalinks({
+      pattern: ':language/:slug'
+    })
+  )
+  .use(
+    dates({
+      dates: [{ key: 'date', format: 'DD/MM/YYYY' }]
+    })
+  )
+  .use(wordcount())
+  .use(
+    layouts({
+      engine: 'handlebars',
+      directory: path.resolve(__dirname, 'src/layouts'),
+      partials: path.resolve(__dirname, 'src/layouts/el')
+    })
+  )
